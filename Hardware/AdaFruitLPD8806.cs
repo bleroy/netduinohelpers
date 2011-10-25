13c888a64c84b0d91d5f94b5065d441250e6969a
Fabien Royer <devnull@localhost>
2011-10-25 00:18:58 -0700

    Updated AdaFruitLPD8806 driver + test sample Added 'VideoStripBmp' bitmap conversion tool

--- Hardware/AdaFruitLPD8806.cs
+++ Hardware/AdaFruitLPD8806.cs
@@ -4,10 +4,24 @@ using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 
 namespace netduino.helpers.Hardware {
+    // Based on https://github.com/adafruit/LPD8806
     public class AdaFruitLPD8806 : IDisposable {
-        int PixelCount { get; set; }
-        public AdaFruitLPD8806(int pixels, Cpu.Pin chipSelect, SPI.SPI_module spiModule = SPI.SPI_module.SPI1, uint speedKHz = 1000) {
-            PixelCount = pixels;
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int PixelCount { get; private set; }
+        public int FrameSize { get; private set; }
+        public const int BytesPerPixel = 3;
+        protected int PixelBufferEnd;
+
+        public AdaFruitLPD8806(int width, int height, Cpu.Pin chipSelect, SPI.SPI_module spiModule = SPI.SPI_module.SPI1, uint speedKHz = 1000) {
+            
+            Width = width;
+            Height = height;
+            PixelCount = Width * Height;
+            PixelBufferEnd = (PixelCount - 1) * BytesPerPixel;
+            FrameSize = Width * Height * BytesPerPixel;
+
             var spiConfig = new SPI.Configuration(
                 SPI_mod: spiModule,
                 ChipSelect_Port: chipSelect,
@@ -21,7 +35,7 @@ namespace netduino.helpers.Hardware {
 
             spi = new SPI(spiConfig);
 
-            pixelBuffer = new byte[PixelCount * 3];
+            pixelBuffer = new byte[PixelCount * BytesPerPixel];
         }
 
         protected SPI spi;
@@ -31,22 +45,27 @@ namespace netduino.helpers.Hardware {
 
         public void Reset() {
             for (var i = 0; i < pixelBuffer.Length; i++) {
-                pixelBuffer[i] = 0;
+                pixelBuffer[i] = 0x80;
             }
         }
 
-        public void Refresh() {
+        public void SetStripColor(UInt32 color) {
+            for (var pixel = 0; pixel < PixelCount; pixel++) {
+                SetPixel(pixel, color);
+            }
+        }
+
+        public void Refresh(int delayMS = 4) {
             spi.Write(attentionSequence);
             spi.Write(pixelBuffer);
             var ledLatchCount = PixelCount * 2;
             for (var i = 0; i < ledLatchCount; i++) {
                 spi.Write(latchSequence);
             }
-            Thread.Sleep(10);
+            Thread.Sleep(delayMS);
         }
 
-        UInt32 RgbToColor(byte r, byte g, byte b)
-        {
+        public UInt32 RgbToColor(byte r, byte g, byte b) {
           // Take the lowest 7 bits of each value and append them end to end
           // We have the top bit set high (its a 'parity-like' bit in the protocol and must be set!
           UInt32 color;
@@ -65,14 +84,80 @@ namespace netduino.helpers.Hardware {
             pixelBuffer[pixelIndex * 3 + 2] = (byte)(b | 0x80);
         }
 
+        // The color parameter needs to come from RgbToColor()
         public void SetPixel(int pixelIndex, UInt32 color) {
             if (pixelIndex >= PixelCount) throw new ArgumentOutOfRangeException("pixelIndex");
-            pixelBuffer[pixelIndex * 3] = (byte)((color >> 16) | 0x80);
-            pixelBuffer[pixelIndex * 3 + 1] = (byte)((color >> 8) | 0x80);
-            pixelBuffer[pixelIndex * 3 + 2] = (byte)(color | 0x80);
+            pixelBuffer[pixelIndex * 3] = (byte)(color >> 16);
+            pixelBuffer[pixelIndex * 3 + 1] = (byte)(color >> 8);
+            pixelBuffer[pixelIndex * 3 + 2] = (byte)(color);
+        }
+
+        public void SetPixel(int x, int y, UInt32 color) {
+            SetPixel((y * Width) + x, color);
+        }
+
+        protected byte[] singlePixelBuffer = new byte[BytesPerPixel];
+
+        public void ShiftRight(bool circular = false) {
+            if (circular) {
+                Array.Copy(pixelBuffer, PixelBufferEnd, singlePixelBuffer, 0, BytesPerPixel);
+                Array.Copy(pixelBuffer, 0, pixelBuffer, BytesPerPixel, PixelBufferEnd);
+                Array.Copy(singlePixelBuffer, pixelBuffer, BytesPerPixel);
+            } else {
+                singlePixelBuffer[0] = 0x80;
+                singlePixelBuffer[1] = 0x80;
+                singlePixelBuffer[2] = 0x80;
+                Array.Copy(pixelBuffer, 0, pixelBuffer, BytesPerPixel, PixelBufferEnd);
+                Array.Copy(singlePixelBuffer, pixelBuffer, BytesPerPixel);
+            }
+        }
+
+        public void ShiftLeft(bool circular = false) {
+            if (circular) {
+                Array.Copy(pixelBuffer, 0, singlePixelBuffer, 0, BytesPerPixel);
+                Array.Copy(pixelBuffer, 3, pixelBuffer, 0, PixelBufferEnd);
+                Array.Copy(singlePixelBuffer, 0, pixelBuffer, PixelBufferEnd, BytesPerPixel);
+            } else {
+                singlePixelBuffer[0] = 0x80;
+                singlePixelBuffer[1] = 0x80;
+                singlePixelBuffer[2] = 0x80;
+                Array.Copy(pixelBuffer, 3, pixelBuffer, 0, PixelBufferEnd);
+                Array.Copy(singlePixelBuffer, 0, pixelBuffer, PixelBufferEnd, BytesPerPixel);
+            }
+        }
+
+        public void Gradient(int startRed, int startGreen, int startBlue, int endRed, int endGreen, int endBlue, int pixelStart, int pixelEnd) {
+            var pixelRange = pixelEnd - pixelStart;
+            for (var pixel = pixelStart; pixel <= pixelEnd; pixel++) {
+                var ratio = (float)pixel / (float)pixelRange;
+                SetPixel(pixel,
+                    (byte)(endRed * ratio + startRed * (1 - ratio)),
+                    (byte)(endGreen * ratio + startGreen * (1 - ratio)),
+                    (byte)(endBlue * ratio + startBlue * (1 - ratio))
+                    );
+            }
+        }
+
+        public void CopyVideoStripBitmap(byte[] bitmap, int sourceBufferOffset = 0) {
+            var length = Width * BytesPerPixel;
+            for (var row = 0; row < Height; row += 2) {
+                var offSet = Width * row * BytesPerPixel;
+                Array.Copy(bitmap, offSet + sourceBufferOffset, pixelBuffer, offSet, length);
+            }
+            for (var row = 1; row < Height; row += 2) {
+                var offSet = Width * row * BytesPerPixel;
+                var targetCount = length - BytesPerPixel;
+                var sourceCount = 0;
+                while (targetCount >= 0) {
+                    Array.Copy(bitmap, sourceBufferOffset + offSet + sourceCount, pixelBuffer, offSet + targetCount, BytesPerPixel);
+                    targetCount -= BytesPerPixel;
+                    sourceCount += BytesPerPixel;
+                }
+            }
         }
 
         public void Dispose() {
+            singlePixelBuffer = null;
             pixelBuffer = null;
             attentionSequence = null;
             latchSequence = null;
---
+++ Tools/VideoStripBmp/VideoStripBmp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace netduino.helpers.tools {
+    class Program {
+        static void Main(string[] args) {
+            var list = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.bmp");
+            foreach(var file in list) {
+                ProcessBitmap(file);
+            }
+        }
+
+        const int BytesPerPixel = 3;
+        private static byte[] pixelBuffer = new byte[BytesPerPixel];
+        const int GreenByte = 0;
+        const int RedByte = 1;
+        const int BlueByte = 2;
+
+        public static void ProcessBitmap(string path) {
+            using(var bmp = new Bitmap(path)) {
+                if(bmp.PixelFormat != System.Drawing.Imaging.PixelFormat.Format24bppRgb) {
+                    Console.WriteLine("Please provide a 24-bit depth bitmap to convert.");
+                    return;
+                }
+                var periodPosition = path.LastIndexOf('.');
+                var filename = path.Substring(0, periodPosition);
+                FileStream bin = new FileStream((string)(filename + ".vsbin"), FileMode.Create, FileAccess.Write);
+                for(int row = 0; row < bmp.Height; row++) {
+                    for(int column = 0; column < bmp.Width; column++) {
+                        var pixel = bmp.GetPixel(column, row);
+                        pixelBuffer[GreenByte] = (byte)(pixel.G | 0x80);
+                        pixelBuffer[RedByte] = (byte)(pixel.R | 0x80);
+                        pixelBuffer[BlueByte] = (byte)(pixel.B | 0x80);
+                        bin.Write(pixelBuffer, 0, BytesPerPixel);
+                        Console.Write("0x{0:x},0x{1:x},0x{2:x},", pixelBuffer[GreenByte], pixelBuffer[RedByte], pixelBuffer[BlueByte]);
+                    }
+                    Console.Write("\r\n");
+                }
+                bin.Close();
+                Console.WriteLine("");
+            }
+        }
+    }
+}
